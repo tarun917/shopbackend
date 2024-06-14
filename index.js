cc85c7fe62e2e3b5f4bd44c28311ecc0d9c0b21a
tarun917 <139982176+tarun917@users.noreply.github.com>
2024-06-14 13:49:59 +0530

    Update index.js
    as like before

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const port = 4000;
 const express = require("express");
-const app = require("express");
+const app = express();
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
 
-
-const express = require('express');
-const cors = require('cors');
+app.use(express.json());
+app.use(cors());
 
 // yahan se start
 app.use(cors({
